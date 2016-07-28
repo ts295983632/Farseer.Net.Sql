63f6ff7e7245fcd01d13cd90fdcb6f75d8e1b209
steden <steden>
2016-07-28 12:47:50 +0800

    修复：IDataReader转实体时，当值类型，目标值为null时，需要做默认值处理

--- Src/_/FS.Extends/TypeConvertExtend.cs
+++ Src/_/FS.Extends/TypeConvertExtend.cs
@@ -20,11 +20,11 @@ namespace FS.Extends
         public static List<TEntity> ToList<TEntity>(this IDataReader reader) where TEntity : class, new()
         {
             var list = new List<TEntity>();
-            var map = SetMapCacheManger.Cache(typeof (TEntity));
+            var map = SetMapCacheManger.Cache(typeof(TEntity));
 
             while (reader.Read())
             {
-                var t = (TEntity) Activator.CreateInstance(typeof (TEntity));
+                var t = (TEntity)Activator.CreateInstance(typeof(TEntity));
 
                 //赋值字段
                 foreach (var kic in map.MapList)
@@ -50,9 +50,9 @@ namespace FS.Extends
         /// <typeparam name="TEntity">实体类</typeparam>
         public static TEntity ToEntity<TEntity>(this IDataReader reader) where TEntity : class, new()
         {
-            var map = SetMapCacheManger.Cache(typeof (TEntity));
+            var map = SetMapCacheManger.Cache(typeof(TEntity));
 
-            var t = (TEntity) Activator.CreateInstance(typeof (TEntity));
+            var t = (TEntity)Activator.CreateInstance(typeof(TEntity));
             var isHaveValue = false;
 
             if (reader.Read())
@@ -64,6 +64,8 @@ namespace FS.Extends
                     {
                         if (!kic.Key.CanWrite) { continue; }
                         var oVal = ConvertHelper.ConvertType(reader[kic.Value.Field.Name], kic.Key.PropertyType);
+                        //  当值类型，目标值为null时，需要做默认值处理
+                        if (oVal == null && !kic.Key.PropertyType.IsGenericType && !kic.Key.PropertyType.IsClass) { oVal = 0; }
                         PropertySetCacheManger.Cache(kic.Key, t, oVal);
 
                         isHaveValue = true;
@@ -83,13 +85,13 @@ namespace FS.Extends
         {
             var dt = new DataTable();
             if (lst.Count == 0) { return dt; }
-            var map = SetMapCacheManger.Cache(typeof (TEntity));
+            var map = SetMapCacheManger.Cache(typeof(TEntity));
             var lstFields = map.MapList.Where(o => o.Value.Field.IsMap).ToList();
             foreach (var field in lstFields)
             {
                 var type = field.Key.PropertyType;
                 // 对   List 类型处理
-                if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof (Nullable<>)) { type = type.GetGenericArguments()[0]; }
+                if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>)) { type = type.GetGenericArguments()[0]; }
                 dt.Columns.Add(field.Value.Field.Name, type);
             }
 
@@ -98,7 +100,7 @@ namespace FS.Extends
                 dt.Rows.Add(dt.NewRow());
                 foreach (var field in lstFields)
                 {
-                    var value = ConvertHelper.GetValue(info, field.Key.Name, (object) null);
+                    var value = ConvertHelper.GetValue(info, field.Key.Name, (object)null);
                     if (value == null) { continue; }
                     if (!dt.Columns.Contains(field.Value.Field.Name)) { dt.Columns.Add(field.Value.Field.Name); }
                     dt.Rows[dt.Rows.Count - 1][field.Value.Field.Name] = value;
@@ -127,7 +129,7 @@ namespace FS.Extends
         public static List<TEntity> ToList<TEntity>(this DataTable dt) where TEntity : class, new()
         {
             var list = new List<TEntity>();
-            var map = SetMapCacheManger.Cache(typeof (TEntity));
+            var map = SetMapCacheManger.Cache(typeof(TEntity));
             foreach (DataRow dr in dt.Rows)
             {
                 // 赋值字段
@@ -190,12 +192,12 @@ namespace FS.Extends
         /// </summary>
         public static List<TEntity> ToList<TEntity>(this XElement element) where TEntity : class
         {
-            var orm = SetMapCacheManger.Cache(typeof (TEntity));
+            var orm = SetMapCacheManger.Cache(typeof(TEntity));
             var list = new List<TEntity>();
 
             foreach (var el in element.Elements())
             {
-                var t = (TEntity) Activator.CreateInstance(typeof (TEntity));
+                var t = (TEntity)Activator.CreateInstance(typeof(TEntity));
 
                 //赋值字段
                 foreach (var kic in orm.MapList)
