79eec2370d5fb97c9025d59180fa77846e5ccf1f
steden <steden>
2016-08-26 12:53:09 +0800

    格式化代码

--- Test/TimeTests/DB/UserDataVO.cs
+++ Test/TimeTests/DB/UserDataVO.cs
@@ -1,34 +1,125 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using FS.Sql.Tests.DB;
-using FS.Sql.Tests.DB.Members;
+using System.Collections.Generic;
 using FS.Utils.Common;
-
-namespace TimeTests.DB
+using FS.Extends;
+namespace FS.Sql.Tests.DB.Members
 {
-    public class UserDataVO : UserVO
+    public class UserVOByDataRow : FS.Sql.Tests.DB.Members.UserVO
     {
-        public UserDataVO() { }
-        public UserDataVO(DataRow dr)
+        public static FS.Sql.Tests.DB.Members.UserVO ConvertDataRow(DataRow dr)
         {
-            if (dr.Table.Columns.Contains("SiteIDs") && dr["SiteIDs"] != null) { base.SiteIDs = ConvertHelper.ConvertType(dr["SiteIDs"], typeof(List<System.Int32>)) as List<System.Int32>; }
-
-            DateTime createAtOut;
-            if (dr[nameof(CreateAt)] != null && DateTime.TryParse(dr[nameof(CreateAt)].ToString(), out createAtOut)) { base.CreateAt = createAtOut; }
-
-            eumGenderType genderTypeOut;
-            if (dr[nameof(GenderType)] != null && Enum.TryParse(dr[nameof(GenderType)].ToString(), out genderTypeOut)) { base.GenderType = genderTypeOut; }
-
-            Int32 idOut;
-            if (dr[nameof(ID)] != null && Int32.TryParse(dr[nameof(ID)].ToString(), out idOut)) { base.ID = idOut; }
-
-            Int32 logCountOut;
-            if (dr[nameof(LogCount)] != null && Int32.TryParse(dr[nameof(LogCount)].ToString(), out logCountOut)) { base.LogCount = logCountOut; }
-
-            base.LoginIP = dr["LoginIP"] as string;
-            base.PassWord = dr["PassWord"] as string;
-            base.UserName = dr[nameof(UserName)] as string;
+            var entity = new FS.Sql.Tests.DB.Members.UserVO();
+            if (dr.Table.Columns.Contains("ID") && dr["ID"] != null)
+            {
+                System.Int32 ID_Out; if (System.Int32.TryParse(dr["ID"].ToString(), out ID_Out)) { entity.ID = ID_Out; }
+            }
+            if (dr.Table.Columns.Contains("UserName") && dr["UserName"] != null)
+            {
+                entity.UserName = dr["UserName"].ToString();
+            }
+            if (dr.Table.Columns.Contains("PassWord") && dr["PassWord"] != null)
+            {
+                entity.PassWord = dr["PassWord"].ToString();
+            }
+            if (dr.Table.Columns.Contains("GenderType") && dr["GenderType"] != null)
+            {
+                entity.GenderType = (eumGenderType)dr["GenderType"].ConvertType(typeof(DB.eumGenderType));
+            }
+            if (dr.Table.Columns.Contains("LoginCount") && dr["LoginCount"] != null)
+            {
+                System.Int32 LoginCount_Out; if (System.Int32.TryParse(dr["LoginCount"].ToString(), out LoginCount_Out)) { entity.LogCount = LoginCount_Out; }
+            }
+            if (dr.Table.Columns.Contains("SiteIDs") && dr["SiteIDs"] != null)
+            {
+                entity.SiteIDs = ConvertHelper.ConvertType(dr["SiteIDs"], typeof(List<System.Int32>)) as List<System.Int32>;
+            }
+            if (dr.Table.Columns.Contains("LoginIP") && dr["LoginIP"] != null)
+            {
+                entity.LoginIP = dr["LoginIP"].ToString();
+            }
+            if (dr.Table.Columns.Contains("GetDate") && dr["GetDate"] != null)
+            {
+                System.DateTime GetDate_Out; if (System.DateTime.TryParse(dr["GetDate"].ToString(), out GetDate_Out)) { entity.GetDate = GetDate_Out; }
+            }
+            if (dr.Table.Columns.Contains("CreateAt") && dr["CreateAt"] != null)
+            {
+                System.DateTime CreateAt_Out; if (System.DateTime.TryParse(dr["CreateAt"].ToString(), out CreateAt_Out)) { entity.CreateAt = CreateAt_Out; }
+            }
+            if (dr.Table.Columns.Contains("UserRole") && dr["UserRole"] != null)
+            {
+            }
+            if (dr.Table.Columns.Contains("OrderID") && dr["OrderID"] != null)
+            {
+                entity.Orders = ConvertHelper.ConvertType(dr["OrderID"], typeof(List<FS.Sql.Tests.DB.Members.OrdersVO>)) as List<FS.Sql.Tests.DB.Members.OrdersVO>;
+            }
+            return entity;
         }
+
+        public static List<FS.Sql.Tests.DB.Members.UserVO> ConvertDataTable(DataTable dt)
+        {
+            var lst = new List<FS.Sql.Tests.DB.Members.UserVO>(dt.Rows.Count);
+            foreach (DataRow dr in dt.Rows)
+            {
+                lst.Add(ConvertDataRow(dr));
+            }
+            return lst;
+        }
+
+        public static FS.Sql.Tests.DB.Members.UserVO ConvertDataReader(IDataReader reader)
+        {
+            var entity = new FS.Sql.Tests.DB.Members.UserVO();
+            var isHaveValue = false;
+            if (reader.Read())
+            {
+                isHaveValue = true;
+                if (reader.HaveName("ID"))
+                {
+                    System.Int32 ID_Out; if (System.Int32.TryParse(reader["ID"].ToString(), out ID_Out)) { entity.ID = ID_Out; }
+                }
+                if (reader.HaveName("UserName"))
+                {
+                    entity.UserName = reader["UserName"].ToString();
+                }
+                if (reader.HaveName("PassWord"))
+                {
+                    entity.PassWord = reader["PassWord"].ToString();
+                }
+                if (reader.HaveName("GenderType"))
+                {
+                    entity.GenderType = (FS.Sql.Tests.DB.eumGenderType)reader["GenderType"].ConvertType(typeof(FS.Sql.Tests.DB.eumGenderType));
+                }
+                if (reader.HaveName("LoginCount"))
+                {
+                    System.Int32 LoginCount_Out; if (System.Int32.TryParse(reader["LoginCount"].ToString(), out LoginCount_Out)) { entity.LogCount = LoginCount_Out; }
+                }
+                if (reader.HaveName("SiteIDs"))
+                {
+                    entity.SiteIDs = ConvertHelper.ConvertType(reader["SiteIDs"], typeof(List<System.Int32>)) as List<System.Int32>;
+                }
+                if (reader.HaveName("LoginIP"))
+                {
+                    entity.LoginIP = reader["LoginIP"].ToString();
+                }
+                if (reader.HaveName("GetDate"))
+                {
+                    System.DateTime GetDate_Out; if (System.DateTime.TryParse(reader["GetDate"].ToString(), out GetDate_Out)) { entity.GetDate = GetDate_Out; }
+                }
+                if (reader.HaveName("CreateAt"))
+                {
+                    System.DateTime CreateAt_Out; if (System.DateTime.TryParse(reader["CreateAt"].ToString(), out CreateAt_Out)) { entity.CreateAt = CreateAt_Out; }
+                }
+                if (reader.HaveName("UserRole"))
+                {
+
+                }
+                if (reader.HaveName("OrderID"))
+                {
+                    entity.Orders = ConvertHelper.ConvertType(reader["OrderID"], typeof(List<FS.Sql.Tests.DB.Members.OrdersVO>)) as List<FS.Sql.Tests.DB.Members.OrdersVO>;
+                }
+            }
+            return isHaveValue ? entity : null;
+        }
+
     }
 }
--- Test/TimeTests/MapingTests.cs
+++ Test/TimeTests/MapingTests.cs
@@ -10,7 +10,6 @@ using System.Reflection;
 using System.Text;
 using FS.Cache;
 using FS.Sql.Internal;
-using TimeTests.DB;
 
 namespace TimeTests
 {
@@ -21,17 +20,22 @@ namespace TimeTests
         {
             Init();
             AddData();
-            new UserDataVO(dt.Rows[0]);
-            SpeedTest.ConsoleTime("硬编码DataTable转实体", 1, () =>
+
+            UserVOByDataRow.ConvertDataRow(dt.Rows[0]);
+            SpeedTest.ConsoleTime("手动实现转换：DataTable转实体", 100, () =>
+            {
+                var lst = UserVOByDataRow.ConvertDataTable(dt);
+            });
+            SpeedTest.ConsoleTime("表达式树委托：DataTable转实体", 100, () =>
             {
                 var lst = new List<UserVO>(dt.Rows.Count);
                 foreach (DataRow item in dt.Rows)
                 {
-                    lst.Add(new UserDataVO(item));
+                    lst.Add(ToInfo<UserVO>(item));
                 }
             });
             dt.ToList<UserVO>();
-            SpeedTest.ConsoleTime("动态编译派生类DataTable转实体", 1, () =>
+            SpeedTest.ConsoleTime("动态编译转换：DataTable转实体", 100, () =>
             {
                 var lst = dt.ToList<UserVO>();
             });
@@ -50,13 +54,38 @@ namespace TimeTests
             dt.Columns.Add(nameof(UserVO.SiteIDs));
         }
 
-        private static void AddData(int count = 10000)
+        private static void AddData(int count = 100)
         {
             for (int i = 0; i < count; i++)
             {
-                var lst = new List<int> {Rand.GetRandom(), Rand.GetRandom(), Rand.GetRandom()};
+                var lst = new List<int> { Rand.GetRandom(), Rand.GetRandom(), Rand.GetRandom() };
                 dt.Rows.Add(DateTime.Now, eumGenderType.Man, i, Rand.GetRandom(), Rand.CreateRandomString(15), Rand.CreateRandomString(32), Rand.CreateRandomString(12), lst.ToString(","));
             }
+        }
+
+        /// <summary>
+        ///     将DataRow转成实体类
+        /// </summary>
+        /// <typeparam name="TEntity">实体类</typeparam>
+        /// <param name="dr">源DataRow</param>
+        public static TEntity ToInfo<TEntity>(DataRow dr) where TEntity : class, new()
+        {
+            var map = SetMapCacheManger.Cache(typeof(TEntity));
+            var t = new TEntity();
+
+            //赋值字段
+            foreach (var kic in map.MapList)
+            {
+                if (!kic.Key.CanWrite) { continue; }
+                var filedName = kic.Value.Field.IsFun ? kic.Key.Name : kic.Value.Field.Name;
+                if (dr.Table.Columns.Contains(filedName))
+                {
+                    var oVal = dr[filedName].ConvertType(kic.Key.PropertyType);
+                    if (oVal == null) { continue; }
+                    PropertySetCacheManger.Cache(kic.Key, t, oVal);
+                }
+            }
+            return t;
         }
     }
 }
